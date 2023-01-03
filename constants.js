b9c132b1a82b9af320d5c354b811a00bd8d43c8a
Christopher Sosa <urri_@hotmail.com>
2023-01-03 16:30:55 -0600

    Creaccion en jwt y instalaccion

--- constants.js
+++ constants.js
@@ -5,10 +5,14 @@ const DB_HOST = "web-personal.hhxnjxu.mongodb.net";
 const API_VERSION = "v1";
 const IP_SERVER = "localhost";
 
+const JTW_SECRET_KEY =
+  "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
 module.exports = {
   DB_USER,
   DB_PASSWORD,
   DB_HOST,
   API_VERSION,
   IP_SERVER,
+  JTW_SECRET_KEY,
 };
