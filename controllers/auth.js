125ca9ae141ff4e0d1a826cc4faddb4a56f4e99c
Christopher Sosa <urri_@hotmail.com>
2023-01-03 13:55:38 -0600

    Cambios en registro

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,21 @@
+const User = require("../models/user");
+
 function register(req, res) {
-  console.log("Se a ejecutado el registro");
+  const { firstname, lastname, email, password } = req.body;
+
+  if (!email) res.status(400).send({ msg: "El correo es obligatorio" });
+  if (!password) res.status(400).send({ msg: "La contraseña es obligatorio" });
+
+  const user = new User({
+    firstname,
+    lastname,
+    email: email.toLowerCase(),
+    role: "user",
+    active: false,
+    password,
+  });
+
+  console.log(user);
 
   res.status(200).send({ msg: "Todo ok" });
 }
