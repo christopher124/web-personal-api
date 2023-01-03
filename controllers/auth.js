428cea5fc4d7f39ba41ea9dac0b421a6fac25d40
Christopher Sosa <urri_@hotmail.com>
2023-01-03 16:31:25 -0600

    Incriptacion de usuario

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,10 +1,11 @@
+const bcrypt = require("bcryptjs");
 const User = require("../models/user");
 
 function register(req, res) {
   const { firstname, lastname, email, password } = req.body;
 
-  if (!email) res.status(400).send({ msg: "El correo es obligatorio" });
-  if (!password) res.status(400).send({ msg: "La contraseña es obligatorio" });
+  if (!email) res.status(400).send({ msg: "El email es obligatorio" });
+  if (!password) res.status(400).send({ msg: "La contraseña es obligatoria" });
 
   const user = new User({
     firstname,
@@ -12,12 +13,19 @@ function register(req, res) {
     email: email.toLowerCase(),
     role: "user",
     active: false,
-    password,
   });
 
-  console.log(user);
+  const salt = bcrypt.genSaltSync(10);
+  const hashPassword = bcrypt.hashSync(password, salt);
+  user.password = hashPassword;
 
-  res.status(200).send({ msg: "Todo ok" });
+  user.save((error, userStorage) => {
+    if (error) {
+      res.status(400).send({ msg: "Error al crear el usuario" });
+    } else {
+      res.status(200).send(userStorage);
+    }
+  });
 }
 
 module.exports = {
