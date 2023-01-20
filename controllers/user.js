36e09b116fa658ca39d1f27af81eef5f10f6eb64
Christopher Sosa <urri_@hotmail.com>
2023-01-19 18:43:39 -0600

    Obtener todos los usuarios

--- controllers/user.js
+++ controllers/user.js
@@ -1,5 +1,6 @@
 const User = require("../models/user");
 
+// funcion para obtener la informacion de usuario logueado
 async function getMe(req, res) {
   const { user_id } = req.user;
 
@@ -11,7 +12,21 @@ async function getMe(req, res) {
     res.status(200).send(response);
   }
 }
+// funcion para obetener todos los usuarios del sistema
+async function getUser(req, res) {
+  const { active } = req.query;
+
+  let response = null;
+
+  if (active === undefined) {
+    response = await User.find();
+  } else {
+    response = await User.find({ active });
+  }
+  res.status(200).send(response);
+}
 
 module.exports = {
   getMe,
+  getUser,
 };
--- router/user.js
+++ router/user.js
@@ -4,4 +4,6 @@ const md_auth = require("../middlewares/authenticated");
 const api = express.Router();
 
 api.get("/user/me", [md_auth.asureAuth], UserController.getMe);
+api.get("/users", [md_auth.asureAuth], UserController.getUser);
+
 module.exports = api;
