007df567fe0b7b870f0bb8ce90bcb20280d4a0e3
Christopher Sosa <urri_@hotmail.com>
2023-01-19 18:24:19 -0600

    Cambios de authenticated

--- middlewares/authenticated.js
+++ middlewares/authenticated.js
@@ -4,13 +4,13 @@ function asureAuth(req, res, next) {
   if (!req.headers.authorization) {
     return res
       .status(403)
-      .send({ msg: "La peticion no tiene la cabecera de autenticacion" });
+      .send({ msg: "La peticion no tiene la cabecera de autenticación" });
   }
 
   const token = req.headers.authorization.replace("Bearer ", "");
 
   try {
-    const payload = jwt.decode(token);
+    const payload = jwt.decoded(token);
 
     const { exp } = payload;
     const currentData = new Date().getTime();
@@ -18,6 +18,7 @@ function asureAuth(req, res, next) {
     if (exp <= currentData) {
       return res.status(400).send({ msg: "El token ha expirado" });
     }
+
     req.user = payload;
     next();
   } catch (error) {
